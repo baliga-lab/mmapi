7f3e8270a1fd1ee29617818220b4f0325af932b8
Wei-ju Wu <weiju.wu@gmail.com>
2020-04-24 16:02:35 -0700

    added regulator_preferred attribute to causal_flows

--- src/app.py
+++ src/app.py
@@ -414,16 +414,17 @@ def causal_flow():
     conn = dbconn()
     cursor = conn.cursor()
     try:
-        cursor.execute("""select bc.name,mut.name,tfs.name,bmt.role,bc_tf.role,bc.cox_hazard_ratio,bgg.num_genes from bc_mutation_tf bmt join biclusters bc on bmt.bicluster_id=bc.id join mutations mut on bmt.mutation_id=mut.id join tfs on bmt.tf_id=tfs.id join bc_tf on bc.id=bc_tf.bicluster_id and tfs.id=bc_tf.tf_id join (select bc.id,count(bg.gene_id) as num_genes from biclusters bc join bicluster_genes bg on bc.id=bg.bicluster_id group by bc.id) as bgg on bc.id=bgg.id""")
+        cursor.execute("""select bc.name,mut.name,tfs.name,g.preferred,bmt.role,bc_tf.role,bc.cox_hazard_ratio,bgg.num_genes from bc_mutation_tf bmt join biclusters bc on bmt.bicluster_id=bc.id join mutations mut on bmt.mutation_id=mut.id join tfs on bmt.tf_id=tfs.id join bc_tf on bc.id=bc_tf.bicluster_id and tfs.id=bc_tf.tf_id join (select bc.id,count(bg.gene_id) as num_genes from biclusters bc join bicluster_genes bg on bc.id=bg.bicluster_id group by bc.id) as bgg on bc.id=bgg.id left join genes g on g.ensembl_id=tfs.name""")
         return jsonify(entries=[{
             'bicluster': bc,
             'mutation': mut,
             'regulator': tf,
+            'regulator_preferred': tf_preferred,
             'mutation_role': MUTATION_ROLES[mut_role],
             'regulator_role': REGULATOR_ROLES[tf_role],
             'hazard_ratio': hratio,
             'num_genes': ngenes
-        } for bc,mut,tf,mut_role,tf_role,hratio,ngenes in cursor.fetchall()])
+        } for bc,mut,tf,tf_preferred,mut_role,tf_role,hratio,ngenes in cursor.fetchall()])
     finally:
         cursor.close()
         conn.close()
